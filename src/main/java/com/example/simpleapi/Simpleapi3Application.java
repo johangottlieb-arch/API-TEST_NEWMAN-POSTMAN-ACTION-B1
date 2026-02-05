e97b6d2c1a3cccbfc961ed5367c5965200ad241f
Hitesh <demo@gmail.com>
2026-02-05 14:34:48 +0530

    final commit

---
+++ src/main/java/com/example/simpleapi/Simpleapi3Application.java
@@ -0,0 +1,13 @@
+package com.example.simpleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Simpleapi3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Simpleapi3Application.class, args);
+    }
+
+}
